d20aa60dbe584e7ec95d4143346f2a08027ebb3d
ankit garg <ankit garg>
2017-02-24 15:42:28 -0500

    Added billing information update functionality

--- src/main/java/edu/controller/BillingInformationController.java
+++ src/main/java/edu/controller/BillingInformationController.java
@@ -22,8 +22,6 @@ public class BillingInformationController {
 
 	@RequestMapping(value="/bill-info/",method=RequestMethod.GET)
 	public List<BillInfo> getAllBills() {
-		System.out.println("-------------------");
-
 		List<BillInfo> billList =new ArrayList<BillInfo>();
 		System.out.println("aaaaaaa"+billList);
 		billList = (List<BillInfo>) billCrudRepo.findAll();
@@ -33,7 +31,6 @@ public class BillingInformationController {
 	@RequestMapping(value="/bill-info/{id}",method=RequestMethod.GET)
 	public BillInfo getBill(@PathVariable("id") Long id) {
 		BillInfo bill=new BillInfo();
-		System.out.println(bill+"-----------"+id);
 		try {
             bill = billCrudRepo.findOne(id);
         } catch (Exception e) {
@@ -41,22 +38,12 @@ public class BillingInformationController {
 //            return e.getMessage();
         }
 		System.out.println("bill :"+bill);
-		/*if(bill == null){
-			String errorMst = "no Bill information available for id " + id;
-            logger.error(errorMst);
-            return errorMst;
-        } *//*else {
-            return movie.getTitle() + " : " + movie.getYear();
-		}*/
-		bill.toString();
-
 		return bill;
 	}
 
 	@RequestMapping(value="/bill-info/",method=RequestMethod.POST)
-	public String createBill(@RequestBody BillInfo bill) {
+	public BillInfo createBill(@RequestBody BillInfo bill) {
 		System.out.println("--====Post Request=====--");
-		//need to add create if not found, if existing dont do anything functionality
 		logger.info("Creating Bill : {}", bill);
 		/* if (billCrudRepo.exists(bill.getId()) {
             logger.error("Unable to create. A User with name {} already exist", user.getName());
@@ -67,17 +54,40 @@ public class BillingInformationController {
 			billCrudRepo.save(bill);
 		} catch (Exception e) {
 			logger.error(e.getMessage());
-			return e.getMessage();
 		}
-		return "creation successful: " + String.valueOf(bill.getId());
+		return bill;
+//		return "creation successful: " + String.valueOf(bill.getId());
 	}
 	@RequestMapping(value="/bill-info/{id}",method=RequestMethod.PUT)
-	public String updateBill() {
-		return "bill info update";
+	public BillInfo updateBill(@PathVariable("id") Long id,@RequestBody BillInfo updateBill) {
+		BillInfo bill= new BillInfo();
+		bill=billCrudRepo.findOne(id);
+		if(bill.getAcc_number()!=updateBill.getAcc_number()){
+			bill.setAcc_number(updateBill.getAcc_number());
+		}
+		if(bill.getAddress()!=updateBill.getAddress()){
+			bill.setAddress(updateBill.getAddress());
+		}
+		if(bill.getBill_rate()!=updateBill.getBill_rate()){
+			bill.setBill_rate(updateBill.getBill_rate());
+		}
+		if(bill.getDept_name()!=updateBill.getDept_name()){
+			bill.setDept_name(updateBill.getDept_name());
+		}
+		if(bill.getProject_name()!=updateBill.getProject_name()){
+			bill.setProject_name(updateBill.getProject_name());
+		}
+		if(bill.getProject_user_email()!=updateBill.getProject_user_email()){
+			bill.setProject_user_email(updateBill.getProject_user_email());
+		}
+		if(bill.getProject_user_name()!=updateBill.getProject_user_name()){
+			bill.setProject_user_name(updateBill.getProject_user_name());
+		}
+        billCrudRepo.save(bill);
+		return bill;
 	}
 	@RequestMapping(value="/bill-info/{id}",method=RequestMethod.DELETE)
-	public String deleteBill() {
+	public String deleteBill(@PathVariable("id") Long id) {
 		return "Greetings from Spring Boot!";
 	}
-
 }
