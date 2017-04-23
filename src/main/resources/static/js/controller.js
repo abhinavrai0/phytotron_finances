0deb80958cb336529a8837e8a7bb8a3ef301d890
agarg9 <agarg9@ncsu.edu>
2017-04-23 15:18:48 -0400

    fixing css

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -190,6 +190,7 @@ billingApp.controller("department_list_Controller", function($rootScope,$scope,$
 			$scope.status = response.data;
 			$log.info(response);
 	});
+	//sort function
 	$scope.sort = function(keyname){
         $scope.sortKey = keyname;   //set the sortKey to the param passed
         $scope.reverse = !$scope.reverse; //if true make it false and vice versa
