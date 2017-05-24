9dd5f32720d01e56de0a394a8ad1510e62a5a1ec
agarg9 <agarg9@ncsu.edu>
2017-05-24 12:40:23 -0400

    filter ip

--- src/main/java/edu/Application.java
+++ src/main/java/edu/Application.java
@@ -2,16 +2,17 @@ package edu;
 
 import org.springframework.boot.SpringApplication;
 import org.apache.catalina.filters.RemoteAddrFilter;
-import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
+//import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+//public class Application extends SpringBootServletInitializer {
+public class Application{
 /*	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
@@ -24,7 +25,7 @@ public class Application extends SpringBootServletInitializer {
 
 
 
-	/*@Bean
+	@Bean
 	 public FilterRegistrationBean remoteAddressFilter() {
 	  FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
 	  RemoteAddrFilter filter = new RemoteAddrFilter();
@@ -36,11 +37,11 @@ public class Application extends SpringBootServletInitializer {
 
 //	  filter.setAllow("10\\.\\d+\\.\\d+\\.\\d+|71\\.69\\.\\d+\\.\\d+|192\\.168\\.\\d+\\.\\d+|2606:a000:4dc9:e500:258a:8709:8650:ff80");
 //	  filter.setAllow("192\\.168\\.0\\.17");
-	  filter.setAllow("192\\.168\\.0\\.22|107.\\77.\\240.\\26");
+	  filter.setAllow("152\\.1\\.177\\.72|152\\.1\\.177\\.43|152\\.1\\.177\\.106");
 //	  filter.setAllow("10\\.255\\.244\\.1");
 //	  <param-value>127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1</param-value>
 	  filterRegistrationBean.setFilter(filter);
 	  filterRegistrationBean.addUrlPatterns("/*");
 	  return filterRegistrationBean;
-	 }*/
+	 }
 }
